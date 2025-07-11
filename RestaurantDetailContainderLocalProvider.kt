850a57e9734683eaff99b7654e934ae2f2071b3e
sryang <sarang628@gmail.com>
2025-07-11 16:49:02 +0800

    update

--- RestaurantDetailContainderLocalProvider.kt
+++ RestaurantDetailContainderLocalProvider.kt
@@ -2,13 +2,19 @@ package com.sarang.torang.di.restaurant_detail_container_di
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.CompositionLocalProvider
+import com.sarang.library.compose.restaurantdetail.review.RestaurantReviews
+import com.sarang.torang.compose.RestaurantGalleryScreen
 import com.sarang.torang.compose.menu.RestaurantMenu
 import com.sarang.torang.compose.menu.RestaurantMenuScreen
 import com.sarang.torang.compose.type.RestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.compose.restaurantdetail.RestaurantOverViewScreen
+import com.sarang.torang.compose.type.LocalRestaurantGalleryImageLoader
 import com.sarang.torang.compose.type.LocalRestaurantOverViewImageLoader
 import com.sarang.torang.compose.type.LocalRestaurantOverviewRestaurantInfo
+import com.sarang.torang.compose.type.RestaurantGalleryInRestaurantDetailContainer
 import com.sarang.torang.compose.type.RestaurantMenuInRestaurantDetailContainer
+import com.sarang.torang.compose.type.RestaurantReviewInRestaurantDetailContainer
+import com.sarang.torang.di.restaurant_gallery_di.restaurantGalleryImageLoader
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewImageLoader
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewRestaurantInfo
 
@@ -24,4 +30,14 @@ val customRestaurantOverviewInRestaurantDetailContainer : RestaurantOverviewInRe
 
 val customRestaurantMenuInRestaurantDetailContainer : RestaurantMenuInRestaurantDetailContainer = {
     RestaurantMenuScreen(restaurantId = 234)
+}
+
+val customRestaurantReviewInRestaurantDetailContainer : RestaurantReviewInRestaurantDetailContainer = {
+
+}
+
+val customRestaurantGalleryInRestaurantDetailContainer : RestaurantGalleryInRestaurantDetailContainer = {
+    CompositionLocalProvider(LocalRestaurantGalleryImageLoader provides restaurantGalleryImageLoader) {
+    RestaurantGalleryScreen(restaurantId = 234)
+    }
 }
