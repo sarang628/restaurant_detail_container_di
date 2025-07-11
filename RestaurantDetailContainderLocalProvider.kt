dff6cb5b584f55452336e059d18d492686319e5e
sryang <sarang628@gmail.com>
2025-07-11 14:34:16 +0800

    update

--- RestaurantDetailContainderLocalProvider.kt
+++ RestaurantDetailContainderLocalProvider.kt
@@ -2,10 +2,13 @@ package com.sarang.torang.di.restaurant_detail_container_di
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.CompositionLocalProvider
-import com.sarang.library.compose.type.RestaurantOverviewInRestaurantDetailContainer
+import com.sarang.torang.compose.menu.RestaurantMenu
+import com.sarang.torang.compose.menu.RestaurantMenuScreen
+import com.sarang.torang.compose.type.RestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.compose.restaurantdetail.RestaurantOverViewScreen
 import com.sarang.torang.compose.type.LocalRestaurantOverViewImageLoader
 import com.sarang.torang.compose.type.LocalRestaurantOverviewRestaurantInfo
+import com.sarang.torang.compose.type.RestaurantMenuInRestaurantDetailContainer
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewImageLoader
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewRestaurantInfo
 
@@ -17,4 +20,8 @@ val customRestaurantOverviewInRestaurantDetailContainer : RestaurantOverviewInRe
     ) {
         RestaurantOverViewScreen(restaurantId = 234)
     }
+}
+
+val customRestaurantMenuInRestaurantDetailContainer : RestaurantMenuInRestaurantDetailContainer = {
+    RestaurantMenuScreen(restaurantId = 234)
 }
---
+++ RestaurantDetailContainerServiceModule.kt
@@ -0,0 +1,21 @@
+package com.sarang.torang.di.restaurant_detail_container_di
+
+import com.sarang.torang.usecase.FindRestaurantNameByRestaurantIdUseCase
+import com.sarang.torang.api.ApiRestaurant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RestaurantDetailContainerServiceModule {
+    @Provides
+    fun provideFindRestaurantNameByRestaurantId(apiRestaurant: ApiRestaurant): FindRestaurantNameByRestaurantIdUseCase {
+        return object : FindRestaurantNameByRestaurantIdUseCase {
+            override suspend fun invoke(restaurantId: Int): String {
+                return apiRestaurant.getRestaurantById(restaurantId).restaurantName
+            }
+        }
+    }
+}
