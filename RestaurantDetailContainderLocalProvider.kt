908ee3a20bbbbe7985c49b7b9b4c8ef6bc84dcda
sryang <sarang628@gmail.com>
2025-07-12 14:14:59 +0800

    update

--- RestaurantDetailContainderLocalProvider.kt
+++ RestaurantDetailContainderLocalProvider.kt
@@ -1,16 +1,20 @@
 package com.sarang.torang.di.restaurant_detail_container_di
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import com.sarang.library.compose.restaurantdetail.review.RestaurantReviews
+import com.sarang.torang.RestaurantNavScreen
 import com.sarang.torang.compose.RestaurantGalleryScreen
-import com.sarang.torang.compose.menu.RestaurantMenu
 import com.sarang.torang.compose.menu.RestaurantMenuScreen
 import com.sarang.torang.compose.type.RestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.compose.restaurantdetail.RestaurantOverViewScreen
 import com.sarang.torang.compose.type.LocalRestaurantGalleryImageLoader
+import com.sarang.torang.compose.type.LocalRestaurantGalleryInRestaurantDetailContainer
+import com.sarang.torang.compose.type.LocalRestaurantMenuInRestaurantDetailContainer
 import com.sarang.torang.compose.type.LocalRestaurantOverViewImageLoader
+import com.sarang.torang.compose.type.LocalRestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.compose.type.LocalRestaurantOverviewRestaurantInfo
+import com.sarang.torang.compose.type.LocalRestaurantReviewInRestaurantDetailContainer
 import com.sarang.torang.compose.type.RestaurantGalleryInRestaurantDetailContainer
 import com.sarang.torang.compose.type.RestaurantMenuInRestaurantDetailContainer
 import com.sarang.torang.compose.type.RestaurantReviewInRestaurantDetailContainer
@@ -39,5 +43,16 @@ val customRestaurantReviewInRestaurantDetailContainer : RestaurantReviewInRestau
 val customRestaurantGalleryInRestaurantDetailContainer : RestaurantGalleryInRestaurantDetailContainer = {
     CompositionLocalProvider(LocalRestaurantGalleryImageLoader provides restaurantGalleryImageLoader) {
     RestaurantGalleryScreen(restaurantId = 234)
+    }
+}
+
+fun provideRestaurantDetailContainer() : @Composable ()->Unit = {
+    CompositionLocalProvider(
+        LocalRestaurantOverviewInRestaurantDetailContainer provides customRestaurantOverviewInRestaurantDetailContainer,
+        LocalRestaurantMenuInRestaurantDetailContainer provides customRestaurantMenuInRestaurantDetailContainer,
+        LocalRestaurantReviewInRestaurantDetailContainer provides customRestaurantReviewInRestaurantDetailContainer,
+        LocalRestaurantGalleryInRestaurantDetailContainer provides customRestaurantGalleryInRestaurantDetailContainer,
+    ) {
+        RestaurantNavScreen(restaurantId = 234)
     }
 }
