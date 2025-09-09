91e64fdbc4a8e75db4cd664abefcd030c9a64b97
sryang <sarang628@gmail.com>
2025-09-09 12:52:16 +0800

    update modules

--- CustomRestaurantMenuInRestaurantDetailContainer.kt
+++ CustomRestaurantMenuInRestaurantDetailContainer.kt
@@ -1,13 +1,17 @@
 package com.sarang.torang.di.restaurant_detail_container_di
 
 import androidx.compose.runtime.CompositionLocalProvider
+import com.sarang.torang.compose.menu.LocalRestaurantMenuImageLoader
 import com.sarang.torang.compose.menu.LocalRestaurantMenuPullToRefresh
 import com.sarang.torang.compose.menu.RestaurantMenuScreen
 import com.sarang.torang.compose.type.RestaurantMenuInRestaurantDetailContainer
 import com.sarang.torang.di.restaurant_menu_di.CustomRestaurantMenuPullToRefresh
+import com.sarang.torang.di.restaurant_menu_di.customRestaurantMenuImageLoader
 
 val customRestaurantMenuInRestaurantDetailContainer : RestaurantMenuInRestaurantDetailContainer = {
-    CompositionLocalProvider(LocalRestaurantMenuPullToRefresh provides CustomRestaurantMenuPullToRefresh){
+    CompositionLocalProvider(LocalRestaurantMenuPullToRefresh provides CustomRestaurantMenuPullToRefresh,
+        LocalRestaurantMenuImageLoader provides customRestaurantMenuImageLoader
+    ){
         RestaurantMenuScreen(restaurantId = it)
     }
 }
--- CustomRestaurantReviewInRestaurantDetailContainer.kt
+++ CustomRestaurantReviewInRestaurantDetailContainer.kt
@@ -1,7 +1,6 @@
 package com.sarang.torang.di.restaurant_detail_container_di
 
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.feed.FeedScreenByRestaurantId
 import com.sarang.torang.compose.feed.LocalPullToRefreshLayoutType
@@ -15,27 +14,13 @@ import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
 import com.sarang.torang.di.feed_di.CustomBottomDetectingLazyColumnType
 import com.sarang.torang.di.feed_di.CustomFeedCompose
 import com.sarang.torang.di.feed_di.CustomPullToRefreshType
-import com.sarang.torang.viewmodels.FeedDialogsViewModel
-import com.sryang.library.pullrefresh.rememberPullToRefreshState
 
 fun customRestaurantReviewInRestaurantDetailContainer(rootNavController: RootNavController) : RestaurantReviewInRestaurantDetailContainer = {
-    val state = rememberPullToRefreshState()
-    val dialogsViewModel: FeedDialogsViewModel = hiltViewModel()
     CompositionLocalProvider(LocalFeedCompose provides CustomFeedCompose,
         LocalBottomDetectingLazyColumnType provides CustomBottomDetectingLazyColumnType,
         LocalPullToRefreshLayoutType provides CustomPullToRefreshType,
         LocalFeedImageLoader provides CustomFeedImageLoader,
         LocalExpandableTextType provides CustomExpandableTextType) {
-        FeedScreenByRestaurantId(restaurantId = it,
-            /*shimmerBrush = { shimmerBrush(it) },
-            feed = provideFeed(dialogsViewModel = dialogsViewModel, navController = rootNavController.navController, rootNavController = rootNavController, videoPlayer = provideVideoPlayer()),
-            pullToRefreshLayout = { isRefreshing, onRefresh, contents ->
-                if (isRefreshing) { state.updateState(RefreshIndicatorState.Refreshing) }
-                else { state.updateState(RefreshIndicatorState.Default) }
-                PullToRefreshLayout(pullRefreshLayoutState = state, refreshThreshold = 80, onRefresh = onRefresh) {
-                    contents.invoke() }
-            },
-            bottomDetectingLazyColumn = provideBottomDetectingLazyColumn()*/
-        )
+        FeedScreenByRestaurantId(restaurantId = it)
     }
 }
