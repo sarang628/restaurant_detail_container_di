cf017193f4585a0b4493c66fd553d3b4c90c35ae
sryang <sarang628@gmail.com>
2025-09-23 08:55:23 +0800

    update feed module

--- CustomRestaurantReviewInRestaurantDetailContainer.kt
+++ CustomRestaurantReviewInRestaurantDetailContainer.kt
@@ -3,22 +3,22 @@ package com.sarang.torang.di.restaurant_detail_container_di
 import androidx.compose.runtime.CompositionLocalProvider
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.feed.FeedScreenByRestaurantId
-import com.sarang.torang.compose.feed.LocalPullToRefreshLayoutType
-import com.sarang.torang.compose.feed.component.LocalBottomDetectingLazyColumnType
-import com.sarang.torang.compose.feed.component.LocalFeedCompose
 import com.sarang.torang.compose.feed.internal.components.LocalExpandableTextType
 import com.sarang.torang.compose.feed.internal.components.LocalFeedImageLoader
+import com.sarang.torang.compose.feed.type.LocalBottomDetectingLazyColumnType
+import com.sarang.torang.compose.feed.type.LocalFeedCompose
+import com.sarang.torang.compose.feed.type.LocalPullToRefreshLayoutType
 import com.sarang.torang.compose.type.RestaurantReviewInRestaurantDetailContainer
 import com.sarang.torang.di.basefeed_di.CustomExpandableTextType
 import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
 import com.sarang.torang.di.feed_di.CustomBottomDetectingLazyColumnType
 import com.sarang.torang.di.feed_di.CustomFeedCompose
-import com.sarang.torang.di.feed_di.CustomPullToRefreshType
+import com.sarang.torang.di.feed_di.customPullToRefresh
 
 fun customRestaurantReviewInRestaurantDetailContainer(rootNavController: RootNavController) : RestaurantReviewInRestaurantDetailContainer = {
     CompositionLocalProvider(LocalFeedCompose provides CustomFeedCompose,
         LocalBottomDetectingLazyColumnType provides CustomBottomDetectingLazyColumnType,
-        LocalPullToRefreshLayoutType provides CustomPullToRefreshType,
+        LocalPullToRefreshLayoutType provides customPullToRefresh,
         LocalFeedImageLoader provides CustomFeedImageLoader,
         LocalExpandableTextType provides CustomExpandableTextType) {
         FeedScreenByRestaurantId(restaurantId = it)
