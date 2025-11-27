a64e51e971b6626ca3b0acd35364172e6d42ac12
sryang <sarang628@gmail.com>
2025-11-27 21:46:43 +0900

    add DialogsBox

--- CustomRestaurantOverviewInRestaurantDetailContainer.kt
+++ CustomRestaurantOverviewInRestaurantDetailContainer.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sarang.torang.FeedDialogsViewModel
 import com.sarang.torang.RestaurantInfoViewModel
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.feed.internal.components.LocalExpandableTextType
@@ -16,28 +17,38 @@ import com.sarang.torang.compose.type.LocalRestaurantOverviewRestaurantInfo
 import com.sarang.torang.compose.type.RestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.di.basefeed_di.CustomExpandableTextType
 import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
-import com.sarang.torang.di.restaurant_overview_di.CustomRestaurantFeedType
 import com.sarang.torang.di.restaurant_overview_di.CustomRestaurantOverviewPullToRefreshType
+import com.sarang.torang.di.restaurant_overview_di.customRestaurantFeedType
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewImageLoader
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewRestaurantInfo
 
+private val tag = "__customRestaurantOverviewInRestaurantDetailContainer"
 @OptIn(ExperimentalMaterial3Api::class)
-fun customRestaurantOverviewInRestaurantDetailContainer(rootNavController: RootNavController) : RestaurantOverviewInRestaurantDetailContainer = {
+fun customRestaurantOverviewInRestaurantDetailContainer(
+    rootNavController: RootNavController,
+    onComment      : (Int) -> Unit    = { Log.w(tag, "onComment callback is not set") },
+    onShare        : (Int) -> Unit    = { Log.w(tag, "onShare callback is not set") },
+    onMenu         : (Int) -> Unit    = { Log.w(tag, "onMenu callback is not set") },
+    onErrorMessage : (String) -> Unit = { }
+) : RestaurantOverviewInRestaurantDetailContainer = { restaurantId ->
     val restaurantInfoViewModel : RestaurantInfoViewModel = hiltViewModel()
     CompositionLocalProvider(
         LocalRestaurantOverViewImageLoader provides restaurantOverViewImageLoader,
         LocalRestaurantOverviewRestaurantInfo provides restaurantOverViewRestaurantInfo(rootNavController, restaurantInfoViewModel),
         LocalFeedImageLoader provides { CustomFeedImageLoader().invoke(it) },
         LocalExpandableTextType provides CustomExpandableTextType,
-        LocalRestaurantFeed provides CustomRestaurantFeedType,
+        LocalRestaurantFeed provides customRestaurantFeedType(onComment = onComment,
+                                                              onShare = onShare,
+                                                              onMenu = onMenu ),
         LocalPullToRefresh provides CustomRestaurantOverviewPullToRefreshType
     ) {
-        RestaurantOverViewScreen(restaurantId = it,
+        RestaurantOverViewScreen(restaurantId = restaurantId,
             onProfile = {rootNavController.profile(it)},
             onContents = { Log.d("__customRestaurantOverviewInRestaurantDetailContainer", "onContents: ${it}"); rootNavController.review(it)},
             onLocation = { Log.d("__customRestaurantOverviewInRestaurantDetailContainer", "onLocation") },
-            onRefresh = { restaurantInfoViewModel.refresh(it) },
-            isRefreshRestaurantInfo = restaurantInfoViewModel.isRefresh
+            onRefresh = { restaurantInfoViewModel.refresh(restaurantId = restaurantId) },
+            isRefreshRestaurantInfo = restaurantInfoViewModel.isRefresh,
+            onErrorMessage = onErrorMessage
         )
     }
 }
--- ProvideRestaurantDetailContainer.kt
+++ ProvideRestaurantDetailContainer.kt
@@ -2,7 +2,9 @@ package com.sarang.torang.di.restaurant_detail_container_di
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
+import com.sarang.torang.FeedDialogsViewModel
 import com.sarang.torang.RestaurantNavScreen
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.menu.LocalRestaurantMenuImageLoader
@@ -10,17 +12,27 @@ import com.sarang.torang.compose.type.LocalRestaurantGalleryInRestaurantDetailCo
 import com.sarang.torang.compose.type.LocalRestaurantMenuInRestaurantDetailContainer
 import com.sarang.torang.compose.type.LocalRestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.compose.type.LocalRestaurantReviewInRestaurantDetailContainer
+import com.sarang.torang.di.dialogsbox_di.ProvideMainDialog
 import com.sarang.torang.di.restaurant_menu_di.customRestaurantMenuImageLoader
 
-fun provideRestaurantDetailContainer(rootNavController: RootNavController): @Composable (NavBackStackEntry)->Unit = {
+fun provideRestaurantDetailContainer(rootNavController: RootNavController,
+                                     onErrorMessage : (String) -> Unit = { }): @Composable (NavBackStackEntry)->Unit = {
     val restaurantId = it.arguments?.getString("restaurantId")
+    val dialogsViewModel : FeedDialogsViewModel = hiltViewModel()
     CompositionLocalProvider(
-        LocalRestaurantOverviewInRestaurantDetailContainer provides customRestaurantOverviewInRestaurantDetailContainer(rootNavController),
+        LocalRestaurantOverviewInRestaurantDetailContainer provides
+                customRestaurantOverviewInRestaurantDetailContainer(rootNavController = rootNavController,
+                                                                    onMenu = dialogsViewModel::onMenu,
+                                                                    onShare = dialogsViewModel::onShare,
+                                                                    onComment = dialogsViewModel::onComment,
+                                                                    onErrorMessage = onErrorMessage),
         LocalRestaurantMenuInRestaurantDetailContainer provides customRestaurantMenuInRestaurantDetailContainer,
         LocalRestaurantReviewInRestaurantDetailContainer provides customRestaurantReviewInRestaurantDetailContainer(rootNavController),
         LocalRestaurantGalleryInRestaurantDetailContainer provides customRestaurantGalleryInRestaurantDetailContainer,
         LocalRestaurantMenuImageLoader provides customRestaurantMenuImageLoader
     ) {
-        RestaurantNavScreen(restaurantId = restaurantId?.toInt() ?: 0, onBack = { rootNavController.popBackStack() })
+        ProvideMainDialog(dialogsViewModel = dialogsViewModel) {
+            RestaurantNavScreen(restaurantId = restaurantId?.toInt() ?: 0, onBack = { rootNavController.popBackStack() })
+        }
     }
 }
